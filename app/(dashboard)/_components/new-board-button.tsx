788b1564f83c725fe02ba259f9dd6a1f758f8a20
GianniCrux <gianni.disti@gmail.com>
2024-04-05 17:28:49 +0200

    Added a searching query method and optimized redirect.

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -6,6 +6,7 @@ import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 interface NewBoardButtonProps {
@@ -17,6 +18,7 @@ export const NewBoardButton = ({
     orgId,
     disabled,
 }: NewBoardButtonProps) => {
+    const router = useRouter(); 
     const { mutate, pending } = useApiMutation(api.board.create);
 
     const onClick = () => {
@@ -26,7 +28,7 @@ export const NewBoardButton = ({
         }) //after creating the new board we will (successfully mutate) we're gonna get the id of that board 
         .then((id) => {
             toast.success("Board created");
-            // TODO: Redirect to /board/{id}
+            router.push(`/board/${id}`);
         })
         .catch(() => toast.error("Failed to create board."));
     }
--- convex/boards.ts
+++ convex/boards.ts
@@ -1,9 +1,13 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
 
+import { getAllOrThrow } from "convex-helpers/server/relationships";
+
 export const get = query({
     args: { //accept the arguments of orgId which is gonna be the required argument
-        orgId: v.string(),
+        orgId: v.string(), // we have to allow arguments other then orgId, that's gonna be search ->
+        search: v.optional(v.string()),
+        favorites: v.optional(v.string()),
     },
     handler: async(ctx, args) => { 
         const identity = await ctx.auth.getUserIdentity();
@@ -12,13 +16,49 @@ export const get = query({
             throw new Error("Unauthorized");
         }
 
-        const boards = await ctx.db
+        if (args.favorites) { 
+            const favoritedBoards = await ctx.db
+                .query("userFavorites")
+                .withIndex("by_user_org", (q) => 
+                    q
+                        .eq("userId", identity.subject)
+                        .eq("orgId", args.orgId)
+                    )
+                    .order("desc")
+                    .collect();
+                    
+                    const ids = favoritedBoards.map((b) => b.boardId);
+
+                    const boards = await getAllOrThrow(ctx.db, ids);
+
+                    return boards.map((board) => ({
+                        ...board,
+                        isFavorite: true,
+                    }));
+        }
+
+        const title = args.search as string; 
+        let boards = []; 
+
+        if (title) {
+            boards = await ctx.db
+                .query("boards")
+                .withSearchIndex("search_title", (q) => 
+                    q
+                        .search("title", title)
+                        .eq("orgId", args.orgId)
+                    )
+                    .collect();    
+        } else { 
+        boards = await ctx.db
         .query("boards") //querying the "boards" collection
         .withIndex("by_org", (q) => q.eq("orgId", args.orgId)) //Filter by index, eq stands for equal to. 
         .order("desc") //Order results in descending order
         .collect(); //Fetch the query results. 
 //(q)=> q.eq("orgId", args.orgId)) this provides the filtering criteria in which q reprents the query builder and the rest of the expression means find documents where the orgId field exactly matches the value in args.orgId
-       
+        }
+
+
         const boardsWithFavoriteRelation = boards.map((board) => {
             return ctx.db
             .query("userFavorites")
