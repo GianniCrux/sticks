1b08d3936afc13e1d2be6de90ea98700144c47ce
GianniCrux <gianni.disti@gmail.com>
2024-04-10 17:32:25 +0200

    Implemented liveblocks and syncrhonized liveblocks with the convex schema, created a skeleton for all the components as a fallback

---
+++ components/room.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ReactNode } from "react"; 
+import { ClientSideSuspense  } from "@liveblocks/react"; //npm package
+
+import { RoomProvider } from "@/liveblocks.config"; //my files
+
+
+interface RoomProps {
+    children: ReactNode;
+    roomId: string;
+    fallback: NonNullable<ReactNode> | null;
+}
+
+export const Room = ({
+    children,
+    roomId,
+    fallback,
+}: RoomProps) => {
+    return (
+        <RoomProvider id={roomId} initialPresence={{}}>
+            <ClientSideSuspense fallback={fallback}>
+                {() => children}
+            </ClientSideSuspense>
+        </RoomProvider>
+    );
+};
